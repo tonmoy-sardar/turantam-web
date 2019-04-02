c814fb1f9aa7178546f1b9455bd09b7c64b5a9ed
Tonmoy Sardar <tonmoy.1984@gmail.com>
2019-04-02 13:21:00 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   //GoogleOAuthClientId: "299801575273",
   //FacebookAppId: "299801575273",
-  apiEndpoint: "http://shyamfuture.com/turantam/api/",
-  imageBaseUrl: "http://shyamfuture.com/turantam/uploads/",
+  apiEndpoint: "http://turantam.com/admin/api/",
+  imageBaseUrl: "http://turantam.com/admin/uploads/",
   paytmEnable :0, // 0 : not enable , 1: enable
   defaultLocation :"Howrah"
 };
